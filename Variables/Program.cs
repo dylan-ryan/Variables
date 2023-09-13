e54238a5e7a9ca8f4227dee4aa1fa4ed8548f3c4
Dylan <dylanryan431@gmail.com>
2023-09-13 14:30:14 -0300

    Update Program.cs

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -10,12 +10,12 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("----------------");
-            Console.WriteLine();
+            
 
             //int = interger whole numbers
+            //float = numbers that arent whole
             //string = text
+
 
             int score;
             int enemyValue;
@@ -23,20 +23,24 @@ namespace Variables
             string GamerTag;
             float percentile;
 
+
             score = 0;
             enemyValue = 200;
             realName = "Dylan Ryan";
             GamerTag = "gamertag";
             percentile = 85.543f;
-
-            
-            
             score = score + enemyValue;
+            
+            
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("----------------");
+            Console.WriteLine();
 
 
             Console.WriteLine(realName + " (" + GamerTag + ")");
             Console.WriteLine("Score: " + score);
             Console.WriteLine("Percentile Rank: " + percentile + "%");
+
 
             Console.WriteLine();
             Console.WriteLine("Press any key to exit");
