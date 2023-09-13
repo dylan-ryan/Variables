583327deae03f58ef93402e5b4f7f979a04ddecb
Dylan <dylanryan431@gmail.com>
2023-09-13 13:38:23 -0300

    Initial commit

---
+++ Variables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(true);
+
+        }
+    }
+}
